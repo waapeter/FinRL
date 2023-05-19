bb0444eb62be7cac28d9a3927b915a2f2ba914f6
waapeter <waapeter@gmail.com>
2023-05-19 21:01:21 +0300

    Ln52: delta attr not exist on Timedelta object.

--- finrl/meta/data_processors/processor_alpaca.py
+++ finrl/meta/data_processors/processor_alpaca.py
@@ -49,7 +49,7 @@ class AlpacaProcessor:
         # from trepan.api import debug;debug()
         # filter opening time of the New York Stock Exchange (NYSE) (from 9:30 am to 4:00 pm) if time_interval < 1D
         day_delta = 86400000000000  # pd.Timedelta('1D').delta == 86400000000000
-        if pd.Timedelta(time_interval).delta < day_delta:
+        if pd.Timedelta(time_interval).total_seconds() < day_delta:
             NYSE_open_hour = "14:30"  # in UTC
             NYSE_close_hour = "20:59"  # in UTC
             data_df = barset.between_time(NYSE_open_hour, NYSE_close_hour)
